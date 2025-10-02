53ff5c48f6e24264cd00119e1eba83ef053d2386
ravil-gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2025-10-02 05:54:45 +0400

    added feature lists by type

--- accident_risk/config.py
+++ accident_risk/config.py
@@ -6,6 +6,18 @@ load_dotenv()
 
 
 TARGET = "accident_risk"
+CATEGORICAL_FEATURES = [
+    "road_type",
+    "lighting",
+    "weather",
+    "road_signs_present",  # binary
+    "public_road",  # binary
+    "time_of_day",
+    "holiday",  # binary
+    "school_season",
+]
+ORDINAL_FEATURES = ["num_lanes", "speed_limit"]
+NUMERICAL_FEATURES = ["curvature", "speed_limit", "num_reported_accidents"]
 
 RAW_TRAIN_PATH = "data/raw/train.csv"
 RAW_TEST_PATH = "data/raw/test.csv"
