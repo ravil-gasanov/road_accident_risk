8ba8a9c0ef7fce68b3466ba14ebb3a34eb98a6b5
ravil-gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2025-10-03 02:53:39 +0400

    added StandardScaler for num features

--- accident_risk/experiments/random_forest.py
+++ accident_risk/experiments/random_forest.py
@@ -1,8 +1,8 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.feature_selection import SelectKBest
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from accident_risk.config import CATEGORICAL_FEATURES, NUMERICAL_FEATURES, ORDINAL_FEATURES
 from accident_risk.experiments.evaluate import eval_with_cv
@@ -12,7 +12,7 @@ from accident_risk.utils import make_experiment_name
 def build_random_forest():
     preprocessor = ColumnTransformer(
         transformers=[
-            ("num", "passthrough", NUMERICAL_FEATURES),
+            ("num", StandardScaler(), NUMERICAL_FEATURES),
             ("ord", "passthrough", ORDINAL_FEATURES),
             ("cat", OneHotEncoder(), CATEGORICAL_FEATURES),
         ]
